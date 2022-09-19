2cab46bd700eac6853cd3e31bdf9da508a8b708e
Nato <ivannatnat0@gmail.com>
2022-09-19 09:32:42 +0800

    Sign In and Sign Up Activity

--- app/src/main/java/com/example/adutucart/SignInActivity.kt
+++ app/src/main/java/com/example/adutucart/SignInActivity.kt
@@ -1,27 +1,29 @@
 package com.example.adutucart
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.adutucart.databinding.ActivityLoginBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.adutucart.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class LoginActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_login)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         firebaseAuth = FirebaseAuth.getInstance()
         binding.textView.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
@@ -43,6 +45,7 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onStart() {
         super.onStart()
 
